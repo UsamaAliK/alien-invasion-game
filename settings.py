eb80e066a3dae7bd5ad19db535414f2a2638091a
Usama Ali <usamaaliawan88@gmail.com>
2024-06-22 03:20:15 +0500

    .

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class settings():
         self.bg_colours=(230,230,230)
         # ship speed
         self.ship_speed_factor=1.5
-        self.ship_limit=2
+        self.ship_limit=3
         # Bullet settings
         self.bullet_speed_factor = 1
         self.bullet_width = 200
