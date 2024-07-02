efaba3fe9dac63f3e514fb17545c12f595c28270
Usama Ali <usamaaliawan88@gmail.com>
2024-07-02 18:55:10 +0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -15,10 +15,13 @@ class Scoreboard():
       # prepare the initial score image
 
       self.prep_score()
+      self.prep_high_score()
+      self.prep_level()
 
 
   def prep_score(self):
-      score_str=str(self.stats.score)
+      rounded_score=int(round(self.stats.score,-1))
+      score_str="SCORE: {:,}".format(rounded_score)
       self.score_image=self.font.render(score_str,True,self.font_color,self.ai_settings.bg_colours)
       self.score_rect=self.score_image.get_rect()
       self.score_rect.right=self.screen_rect.right-20
@@ -26,6 +29,32 @@ class Scoreboard():
 
   def show_score(self):
       self.screen.blit(self.score_image,self.score_rect)
+      self.screen.blit(self.high_score_image,self.high_score_rect)
+      self.screen.blit(self.level_image,self.level_rect)
+
+
+  def prep_high_score(self):
+      round_high_score=int(round(self.stats.high_score,-1))
+      high_score_str="HIGH SCORE: {:,}".format(round_high_score)
+      self.high_score_image=self.font.render(high_score_str,True,self.font_color,self.ai_settings.bg_colours)
+
+      # center the image of high score
+      self.high_score_rect = self.high_score_image.get_rect()
+      self.high_score_rect.centerx = self.screen_rect.centerx
+      self.high_score_rect.top = self.score_rect.top
+
+
+
+
+
+  def prep_level(self):
+
+   self.level_image=self.font.render("LEVEL: " +str(self.stats.level),True,self.font_color,self.ai_settings.bg_colours)
+
+
+   self.level_rect=self.level_image.get_rect()
+   self.level_rect.right=self.score_rect.right
+   self.level_rect.top=self.score_rect.bottom+10
 
 
 
