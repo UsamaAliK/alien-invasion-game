a99bfad1e64e6cfc3b65804e36c5441ec8dbbcb0
Usama Ali <usamaaliawan88@gmail.com>
2024-07-03 14:36:02 +0500

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,8 +1,11 @@
 import pygame
-class Ship():
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
 
 
     def __init__(self,ai_settings,screen):
+        super(Ship,self).__init__()
         #initialize the ship and set its starting position
         self.screen=screen
         self.ai_settings=ai_settings
